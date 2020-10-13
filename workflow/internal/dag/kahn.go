16e7f16f7b0c138c0cade2872c12a3bfc8247e4c
Soule BA <bah.soule@gmail.com>
2020-10-14 00:17:50 +0200

    refactor: refactoring topological sort function

--- workflow/internal/dag/kahn.go
+++ workflow/internal/dag/kahn.go
@@ -2,24 +2,43 @@ package dag
 
 import "fmt"
 
-// TopOrder is a topological ordering.
-type TopOrder []Vertex
+// TopoOrder is a topological ordering.
+type TopoOrder []Vertex
 
-// TopologicalSort implements the kahn algorithm and returns a topological sort of the graph.
+// TopologicalSort implements the kahn algorithm and returns a topological generateTopo of the graph.
 // The graph must not have any cycle or it will preturn an error.
+// TO DO: return a TopoOrder
 func TopologicalSort(graph *Graph) {
-	topOrder := make([]string, 0, len(graph.adjacencyMap))
-	queue := make([]string, 0, len(graph.adjacencyMap))
 	indegreeMap := make(map[string]int)
-	var visited int
-	processIndegrees(&indegreeMap, graph)
+	processIndegrees(indegreeMap, graph)
 
+	topOrder, visited := generateTopo(graph, indegreeMap)
+
+	if visited != len(graph.adjacencyMap) {
+		// TO DO: add a cycle detection
+		fmt.Println("There exists a cycle in the graph")
+	}
+
+	fmt.Println(topOrder)
+
+}
+
+func initQueue(graph *Graph, indegreeMap map[string]int) []string {
+	queue := make([]string, 0, len(graph.adjacencyMap))
 	for vertex := range graph.adjacencyMap {
 		v := vertex.(string)
 		if indegreeMap[v] == 0 {
 			queue = append(queue, v)
 		}
 	}
+	return queue
+}
+
+func generateTopo(graph *Graph, indegreeMap map[string]int) ([]string, int) {
+	topOrder := make([]string, 0, len(graph.adjacencyMap))
+	var visited int
+
+	queue := initQueue(graph, indegreeMap)
 
 	for queue != nil {
 		source := queue[0]
@@ -39,16 +58,10 @@ func TopologicalSort(graph *Graph) {
 			}
 		}
 	}
-
-	if visited != len(graph.adjacencyMap) {
-		fmt.Println("There exists a cycle in the graph")
-	}
-
-	fmt.Println(topOrder)
-
+	return topOrder, visited
 }
 
-func processIndegrees(indegreeMap *map[string]int, graph *Graph) {
+func processIndegrees(indegreeMap map[string]int, graph *Graph) {
 	/*
 			for each node in Nodes
 		    indegree[node] = 0;
@@ -57,11 +70,11 @@ func processIndegrees(indegreeMap *map[string]int, graph *Graph) {
 	*/
 
 	for source, edges := range graph.adjacencyMap {
-		if _, ok := (*indegreeMap)[source.(string)]; ok == false {
-			(*indegreeMap)[source.(string)] = 0
+		if _, ok := indegreeMap[source.(string)]; ok == false {
+			indegreeMap[source.(string)] = 0
 		}
 		for target := range edges {
-			(*indegreeMap)[target.(string)]++
+			indegreeMap[target.(string)]++
 		}
 	}
 
